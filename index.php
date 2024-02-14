1ba3ba4e5c47d851769fe8c65dc031636d49d245
cassevven <119555884+cassevven@users.noreply.github.com>
2024-02-14 14:12:43 -0800

    Submission for Assignment 6.2

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <h3>Question 1</h3>
 <?php
-  // Question 1
-  echo "Hello, World <br />"
+  // Question 1 //
+  echo "Hello, World <br />";
   echo "Hello back";
 ?>
 
@@ -9,27 +9,27 @@
 <h3>Question 2</h3>
 
 <?php
-  // Question 2
+  // Question 2 //
   $greeting = "Good morning";
-  echo $Greeting;
+  echo $greeting;
 ?>
 
 <hr />
 <h3>Question 3</h3>
 
 <?php
-  // Question 3
+  // Question 3 //
   $firstName = "John";
   $lastName = "Doe";
-  echo "Name: " + $firstName + " " + $lastName;
+  echo "Name: " . $firstName . " " . $lastName;
 ?>
 
 <hr />
 <h3>Question 4</h3>
 
 <?php
-  // Question 4
-  $colors = array("Red", "Green", "Blue";
+  // Question 4 //
+  $colors = array("Red", "Green", "Blue");
   echo $colors[1];
 ?>
 
@@ -37,11 +37,11 @@
 <h3>Question 5</h3>
 
 <?php
-  // Question 5
+  // Question 5 //
   function greet($name) {
     echo "Hello, " . $name;
   }
-  greet();
+  greet("Charli");
 ?>
 
 <hr />
@@ -50,7 +50,7 @@
 <?php
   // Question 6: Be careful, this will not result in a loud error, it's just going to be wrong. Read the code to figure out what it SHOULD do, and make sure it does that.
   $age = 20;
-  if ($age = 18) {
+  if ($age == 18) {
       echo "You are 18 years old.";
   } else {
     echo "You are not 18 years old.";
@@ -61,36 +61,36 @@
 <h3>Question 7</h3>
 
 <?php
-  // Question 7
+  // Question 7 //
   $count = 0;
-  echo 'Count: ' + ++$count;
+  echo 'Count: ' . ++$count;
 ?>
 
 <hr />
 <h3>Question 8</h3>
 
 <?php
-  // Question 8
+  // Question 8 //
   define("GREETING", "Hello, everyone.");
-  echo GREETINGS;
+  echo ("GREETINGS");
 ?>
 
 <hr />
 <h3>Question 9</h3>
 
 <?php
-  // Question 9: this test will also not generate an error, but there is something wrong with it.
+  // Question 9: this test will also not generate an error, but there is something wrong with it. //
   function sayHello() {
     echo "Hello!";
   }
-  sayHello("John");
+  sayHello(" ");
 ?>
 
 <hr />
 <h3>Question 10</h3>
 
 <?php
-  // Question 10
+  // Question 10 //
   $user = array("name" => "John Doe", "age" => 30);
-  echo $user[name];
+  echo $user["name"];
 ?>
